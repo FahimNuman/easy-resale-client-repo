0df99e22a4619e3d5a4fc4380a18f07fd711bec8
Md.Abdullah Al Numan Fahim <fahiomnuman87@gmail.com>
2022-11-24 14:47:40 +0600

    header and footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,109 @@
-import React from 'react';
+import React from "react";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 
 const Footer = () => {
     return (
-        <div>
-            this is footer
+        <div className="footer bg-[#b14666] py-8">
+            <div className="container mx-auto mb-5">
+                <div className="footer__section grid md:grid-cols-3 gap-10">
+                    <div className="item">
+                        <div className="logo__section">
+                            <div className="logo__section w-80 flex justify-between items-center mb-4">
+                                <Link to={"/"}>
+                                    <Link to='/'><h1 className="bg-[#b5bdcf]">Easy-Resale</h1></Link>
+                                </Link>
+                            </div>
+                            <div>
+                                <p className="text-slate-300 text-sm">
+                                    This website Build ReactJs, ExpressJS, NodeJS, MongoDB &
+                                    Tailwind CSS. If you have any question you can visit linkdin:<ul>{"https://www.linkedin.com/in/fahim-numan-1587a3161/ "}</ul> or call :
+                                    {"01918647966 "}
+                                    or email: fahiomnuman87@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="logo__section">
+                            <h3 className="text-slate-100 text-lg font-semibold">
+                                Important Link
+                            </h3>
+                            <ul className="mt-3">
+                                <li className="text-slate-300 text-sm">
+                                    <Link to={"/"} className="inline-block py-1">
+                                        <BsFillArrowRightCircleFill className="inline text-[#FBBF24] mb-1 mr-2" />
+                                        Home
+                                    </Link>
+                                </li>
+                                <li className="text-slate-300 text-sm">
+                                    <Link to={"/all-ads/1"} className="inline-block py-1">
+                                        <BsFillArrowRightCircleFill className="inline text-[#FBBF24] mb-1 mr-2" />
+                                        All Ads
+                                    </Link>
+                                </li>
+
+                                <li className="text-slate-300 text-sm">
+                                    <Link to={"/profile"} className="inline-block py-1">
+                                        <BsFillArrowRightCircleFill className="inline text-[#FBBF24] mb-1 mr-2" />
+                                        Post Your Ads
+                                    </Link>
+                                </li>
+                                <li className="text-slate-300 text-sm">
+                                    <Link to={"/profile"} className="inline-block py-1">
+                                        <BsFillArrowRightCircleFill className="inline text-[#FBBF24] mb-1 mr-2" />
+                                        My Account
+                                    </Link>
+                                </li>
+                                <div className="footer__copywriter">
+                                    <div className="container mx-auto">
+                                        <div className="flex justify-center items-center mt-5">
+                                            <p className="text-slate-300 text-sm">
+                                                © 2022 easy-resale. Design & developed with by
+
+                                                Fahim Numan
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="logo__section">
+                            <h3 className="text-slate-100 text-lg font-semibold ">
+                                Newsletter
+                            </h3>
+                            <p className="text-slate-300 text-sm mt-3">
+                                Sign up and receive the latest tips via email.
+                            </p>
+                            <label class="block mt-2">
+                                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-300">
+                                    Email
+                                </span>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                                    placeholder="you@example.com"
+                                />
+                            </label>
+
+                            <Link
+                                href=""
+                                className="btn-primary inline-block py-2 px-8 rounded-lg bg-[#7e22ce] text-slate-200 hover:text-slate-50"
+                            >
+                                Subscribe
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            
+            
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            this is navbar
+        <div className="navbar bg-[#b14666] ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link>All Ads</Link></li>
+                        <li tabIndex={0}>
+                            
+                        </li>
+                        <Link><Link>Post Your Ads</Link></Link>
+                    </ul>
+                </div>
+                <Link className="btn btn-ghost normal-case text-xl">easy-resale</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    <li><Link>All Ads</Link></li>
+                    <li tabIndex={0}>
+                        <Link>
+                            Parent
+                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                        </Link>
+                       
+                    </li>
+                    <li><Link>Post Your Ads</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link className="btn">My Account</Link>
+            </div>
         </div>
     );
 };
